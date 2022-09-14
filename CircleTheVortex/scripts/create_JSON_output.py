edf615e7f66d20c2a451e8755752627330282511
Ramanakumar Sankar <rsankar@umn.edu>
2022-09-14 15:57:11 -0500

    fixed JSON output creation script

--- CircleTheVortex/scripts/create_JSON_output.py
+++ CircleTheVortex/scripts/create_JSON_output.py
@@ -1,5 +1,11 @@
-from aggregation import Aggregator
+import os.path
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+try:
+    from aggregation import Aggregator
+except Exception:
+    pass
 
 
-aggregator = Aggregator('reductions/shape_reducer_hdbscan.csv')
+aggregator = Aggregator('reductions/shape_reducer_hdbscan_reductions.csv')
 aggregator.save_JSON('reductions/data.json')

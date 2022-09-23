857f184a92b091bf6241b0773076fbe20a660304
Ramanakumar Sankar <rsankar@umn.edu>
2022-09-23 15:28:14 -0500

    added multiprocessing to vortex clustering

--- CircleTheVortex/scripts/do_aggregation.sh
+++ CircleTheVortex/scripts/do_aggregation.sh
@@ -9,4 +9,8 @@ panoptes_aggregation reduce ../extracts/shape_extractor_ellipse_extractions.csv 
 	../configs/Reducer_config_workflow_21832_V1.1_shape_extractor_ellipse.yaml -c 12;
 
 cd ..
+# aggregate and convert to JSON
 python3 scripts/create_JSON_output.py
+
+# create vortex clusters
+python3 scripts/find_vortex_clusters.py

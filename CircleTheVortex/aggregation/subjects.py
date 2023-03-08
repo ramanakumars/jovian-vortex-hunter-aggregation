f1aeb6bfa00c92abc28c8d4ce86146d351b4321d
Ramanakumar Sankar <rsankar@umn.edu>
2023-03-08 15:21:56 -0600

    modified subjects class for faster access to subject data

--- CircleTheVortex/aggregation/subjects.py
+++ CircleTheVortex/aggregation/subjects.py
@@ -15,6 +15,15 @@ class SubjectLoader:
         subject_IDs, ind = np.unique(
             self.raw_data['subject_id'], return_index=True)
 
+        self.subject_IDs = []
+        self.latitudes = []
+        self.longitudes = []
+        self.perijoves = []
+        self.urls = []
+        self.classification_counts = []
+        self.retired_ats = []
+        self.retirement_reasons = []
+
         # for each subject, add the metadata from the CSV file
         for i, subject_id in enumerate(tqdm.tqdm(subject_IDs,
                                                  desc='Loading subject data', ascii=True)):
@@ -35,19 +44,28 @@ class SubjectLoader:
             location = ast.literal_eval(row[5])["0"]
 
             # add this to the list
-            self.data.append({'subject_id': row[0],
-                              'latitude': latitude,
-                              'longitude': longitude,
-                              'perijove': perijove,
-                              'url': location,
-                              'classification_count': row[6],
-                              'retired_at': row[7],
-                              'retirement_reason': row[8]})
+            self.subject_IDs.append(row[0])
+            self.latitudes.append(latitude)
+            self.longitudes.append(longitude)
+            self.perijoves.append(perijove)
+            self.urls.append(location)
+            self.classification_counts.append(row[6])
+            self.retired_ats.append(row[7])
+            self.retirement_reasons.append(row[8])
+
+        self.subject_IDs = np.asarray(self.subject_IDs)
+        self.latitudes = np.asarray(self.latitudes)
+        self.longitudes = np.asarray(self.longitudes)
+        self.perijoves = np.asarray(self.perijoves)
+        self.urls = np.asarray(self.urls)
+        self.classification_counts = np.asarray(self.classification_counts)
+        self.retired_ats = np.asarray(self.retired_ats)
+        self.retirement_reasons = np.asarray(self.retirement_reasons)
 
     def get_meta(self, subject_id):
         '''
             Get the latitude/latitude/perijove for a given subject
         '''
-        rowi = next(filter(lambda d: d['subject_id'] ==
-                           subject_id, self.data))
-        return rowi['longitude'], rowi['latitude'], rowi['perijove']
+        ind = np.where(self.subject_IDs == subject_id)[0][0]
+
+        return self.longitudes[ind], self.latitudes[ind], self.perijoves[ind]
